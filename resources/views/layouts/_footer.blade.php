815b9cdb52cf76892a797ee3b440b318343a742c
tomming <233238526@qq.com>
2018-02-02 15:45:16 +0800

    Initial commit

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由<a href="http://www.drupai.com">助派网络制作</a><span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+        <p class="pull-right"><a href="mailto:233238526@qq.com">联系我们</a></p>
+    </div>
+</footer>
