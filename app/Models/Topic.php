d84eeac9745811c925f3d3475660de78edbcc2cc
vagrant <vagrant@homestead>
2018-02-08 03:24:43 +0000

    Topic order

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -13,4 +13,26 @@ class Topic extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+    public function scopeWithOrder($query,$order)
+    {
+        switch ($order){
+            case 'rencent':
+                $query = $this->recent();
+                break;
+            default:
+                $query = $this->recentReplied();
+                break;
+        }
+        return $query->with('user','category');
+    }
+
+    public function scopeRecent($query)
+    {
+        return $query->orderBy('created_at','desc');
+    }
+
+    public function scopeRecentReplied($query){
+        return $query->orderBy('updated_at','desc');
+    }
 }
